6db8dc297bb5079485bb9c1dc03d7146096233a4
bana118 <tachibana.k.ae@m.titech.ac.jp>
2019-02-04 16:52:42 +0900

    mdファイルをアップロードし、モデルのpkをディレクトリ名、ファイル名に自動でするようにした

--- banaTECH/blog/models.py
+++ banaTECH/blog/models.py
@@ -1,10 +1,34 @@
 from django.db import models
-from django.db.models import CharField, FileField, DateTimeField
+from django.db.models import CharField, FileField, DateTimeField, IntegerField
 from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save
+import os.path
 
 # Create your models here.
-
+def md_file_path(instance, filename):
+    fn, ext = os.path.splitext(filename)
+    return "article/{id}/{id}{ext}".format(id=instance.pk, ext=ext)
 class Article(models.Model):
     title = CharField(max_length=128)
-    article = models.FileField(upload_to="article/")
+    article = models.FileField(upload_to=md_file_path)
     post_date = DateTimeField(default=timezone.now)
+
+
+_UNSAVED_FILEFIELD = 'unsaved_filefield'
+
+@receiver(pre_save, sender=Article)
+def skip_saving_file(sender, instance, **kwargs):
+    if not instance.pk and not hasattr(instance, _UNSAVED_FILEFIELD):
+        setattr(instance, _UNSAVED_FILEFIELD, instance.article)
+        instance.article = None
+
+@receiver(post_save, sender=Article)
+def save_file(sender, instance, created, **kwargs):
+    if created and hasattr(instance, _UNSAVED_FILEFIELD):
+        instance.article = getattr(instance, _UNSAVED_FILEFIELD)
+        instance.save()
+        instance.__dict__.pop(_UNSAVED_FILEFIELD)
+
+
+
