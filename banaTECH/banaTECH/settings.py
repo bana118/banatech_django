bc312f0893d7f00eca80e2fe50eea6b6efacb99f
bana118 <tachibana.k.ae@m.titech.ac.jp>
2019-02-24 18:17:42 +0900

    static接続テストファイル設置

--- banaTECH/banaTECH/settings.py
+++ banaTECH/banaTECH/settings.py
@@ -123,7 +123,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
