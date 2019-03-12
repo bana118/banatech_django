560c7f162c665fab72acf75a38e68cba8d208646
bana118 <tachibana.k.ae@m.titech.ac.jp>
2019-03-12 17:58:21 +0900

    URL修正

--- banaTECH/banaTECH/urls.py
+++ banaTECH/banaTECH/urls.py
@@ -14,16 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    re_path(r'^robots\.txt', TemplateView.as_view(template_name='static/robots/robots.txt', content_type='text/plain')),
-    re_path(r'^sitemap\.xml', TemplateView.as_view(template_name='static/sitemap/sitemap.xml', content_type='text/plain')),
     path('', include('frame.urls')),
     path('blog/', include('blog.urls')),
     path('CSSFrameworks/', include('CSSFrameworks.urls')),
