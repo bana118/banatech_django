9bc69c34e4b2836b63c2a29c3ed7bf3192e48be9
bana118 <tachibana.k.ae@m.titech.ac.jp>
2019-04-27 17:06:53 +0900

    canvasに文字を書けるようにした。getImageDataがうまく動かない

--- banaTECH/reiwa/views.py
+++ banaTECH/reiwa/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
 # Create your views here.
+
 
 def reiwa(request):
     return render(request, "reiwa.html")
 
+
 def solo(request):
     return render(request, "solo.html")
+
